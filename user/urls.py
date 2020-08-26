6643ab50156a0bd8601b681cf12c8a32041a5b28
hodongman <jhd9206@gmail.com>
2020-08-27 03:05:06 +0900

    login update

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import UserList, UserDetail, UserViewSet
+from .views import UserList, UserDetail, UserViewSet, UserLoginView
 
 urlpatterns = [
     path("", UserList.as_view(), name = UserList.name),
     path("<int:pk>/", UserDetail.as_view(), name = UserDetail.name),
     path("viewset/", UserViewSet.as_view({'get': 'list'}), name = UserViewSet.name),
+    path("signup/", UserLoginView.as_view(), name = UserLoginView.name)
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserLoginSerializer
 #view를 작성하는 대신 viewset클래스에 공통된 기능을 그룹화 시켜 만듦
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -21,3 +23,24 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     name = 'user-detail'
+
+
+class UserLoginView(generics.RetrieveAPIView):
+
+    permission_classes = (AllowAny,)
+    serializer_class = UserLoginSerializer
+    name = 'user-signup'
+
+    def post(self, request):
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = {
+            'success' : 'True',
+            'status code' : status.HTTP_200_OK,
+            'message': 'User logged in  successfully',
+            'token' : serializer.data['token'],
+            }
+        status_code = status.HTTP_200_OK
+
+        return Response(response, status=status_code)
